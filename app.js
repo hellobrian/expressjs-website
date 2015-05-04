0f202de967d3af06d63e412b992a1fc30f7b8b05
thisisbrianhan <bthan@us.ibm.com>
2015-05-04 15:27:31 -0500

    Add comments for using dust templates

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var app = express();
+// var path = require('path');
+// var cons = require('consolidate');
+// var dust = require('dustjs-linkedin');
+
+// app.set('views', path.join(__dirname, 'views'));
+// app.engine('dust', cons.dust);
+// app.set('view engine', 'dust');
 
 app.use(express.static(__dirname + '/', { extensions: ['html'] }));
 app.use(express.static(__dirname + 'public'));
 
 app.get('/', function(req, res) {
   res.sendFile('index.html');
+  // res.render('index', { layout: 'main-template' });
+});
+
+app.get('/about', function(req, res) {
+  res.sendFile('about.html');
+  // res.render('about', { layout: 'main-template' });
 });
 
 var host = (process.env.VCAP_APP_HOST || 'localhost');
